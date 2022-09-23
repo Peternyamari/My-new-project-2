4aa5df55b499b6d3a2c30ddfac9edeac7b3bb527
Peternyamari <113664817+Peternyamari@users.noreply.github.com>
2022-09-23 08:34:40 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char charactername[] = "Peter Mogeni Nyamari";
+    char charactercity[] = "Nairobi";
+    int characterage = '5';
+    printf("There once lived an old man.\n");
+    printf("His name was %s.\n" ,charactername);
+    printf("The man lived in %s.\n",charactercity);
+    printf("The man was %d years old.\n" ,characterage);
+
+    return 0;
+}
